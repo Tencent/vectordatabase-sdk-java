66b3f59db753eddbe0c733ffb3712af850927aa7
yihaoan <yihaoan@tencent.com>
2023-12-04 16:10:17 +0800

    func(ai search): 支持ai

--- examples/src/main/java/com/tencent/tcvectordb/Main.java
+++ examples/src/main/java/com/tencent/tcvectordb/Main.java
@@ -12,14 +12,14 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-//        System.out.println("----------------------------------------------------------");
-//        System.out.println("------------------------- example ------------------------");
-//        System.out.println("----------------------------------------------------------");
-//        com.tencent.tcvectordb.VectorDBExample.example();
-//        System.out.println("----------------------------------------------------------");
-//        System.out.println("----------------- example with embedding -----------------");
-//        System.out.println("----------------------------------------------------------");
-//        com.tencent.tcvectordb.VectorDBExampleWithEmbedding.example();
+        System.out.println("----------------------------------------------------------");
+        System.out.println("------------------------- example ------------------------");
+        System.out.println("----------------------------------------------------------");
+        com.tencent.tcvectordb.VectorDBExample.example();
+        System.out.println("----------------------------------------------------------");
+        System.out.println("----------------- example with embedding -----------------");
+        System.out.println("----------------------------------------------------------");
+        com.tencent.tcvectordb.VectorDBExampleWithEmbedding.example();
         System.out.println("----------------------------------------------------------");
         System.out.println("----------------- example with ai doc -----------------");
         System.out.println("----------------------------------------------------------");

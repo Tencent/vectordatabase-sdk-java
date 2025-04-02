5f64997356158b4f3834f8981ca03ca73f865bc6
yihaoan <yihaoan@tencent.com>
2025-04-02 19:49:11 +0800

    count consitency version

--- tcvectordb/src/main/java/com/tencent/tcvectordb/examples/CommonService.java
+++ tcvectordb/src/main/java/com/tencent/tcvectordb/examples/CommonService.java
@@ -42,8 +42,8 @@ public class CommonService {
      * @return {@link ConnectParam}
      */
     private static ConnectParam initConnectParam() {
-        String vdbURL = "http://lb-rz3tigrs-971c4fayxj2hsidv.clb.ap-guangzhou.tencentclb.com:60000";
-        String vdbKey = "Du4cacE83Mw2Me5pqkArajUTvqzwrdsDPh0ap1NF";
+        String vdbURL = "";
+        String vdbKey = "";
         System.out.println("\tvdb_url: " + vdbURL);
         System.out.println("\tvdb_key: " + vdbKey);
         return ConnectParam.newBuilder()
